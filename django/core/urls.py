eed9cee23536f3bbf0fd584b517c565f85a40265
Jerome Gill <jerome2.gill@gmail.com>
2024-04-28 15:14:49 +0100

    Remove document module - it's not needed for now

--- django/core/urls.py
+++ django/core/urls.py
@@ -15,15 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
-from document import views
-
-router = routers.DefaultRouter()
-router.register('documents', views.DocumentView, 'document')
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("document/", include("document.urls")),
-    path('api/', include(router.urls)),
 ]
--- django/document/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Document
-
-class DocumentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description')
-
-admin.site.register(Document, DocumentAdmin)
