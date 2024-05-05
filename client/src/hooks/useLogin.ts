6d9acd78a52c278ce0bb91c52d799ea93f8576d8
Jerome Gill <jerome2.gill@gmail.com>
2024-05-05 17:58:34 +0100

    Fixed a bug in login using the wrong kind of redirect. There are several options for redirecting and so I left a comment for if I get round to taking a closer look

--- client/src/hooks/useLogin.ts
+++ client/src/hooks/useLogin.ts
@@ -1,11 +1,14 @@
 import { useState } from 'react';
-import { redirect } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { AuthProvider } from '@/auth/AuthProvider';
 
 export const useLogin = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  // @todo the docs suggest using redirect in loaders and actions instead of this hook
+  // https://reactrouter.com/en/main/hooks/use-navigate
+  const navigate = useNavigate();
 
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -15,7 +18,7 @@ export const useLogin = () => {
       const response = await AuthProvider.signin(username, password);
       if (response.ok) {
         // this will need to redirect to the page the user was trying to access if we add more routes
-        redirect("/")
+        navigate("/")
       } else {
         // Handle each error case. Maybe use one of the react query libraries to handle this
         setError('Invalid username or password');
